232367905b00b61b4908dd3028c68c977511826a
姚琪 <yaoqi@liepin.com>
2019-09-25 19:05:23 +0800

    添加注释

--- src/main/java/com/yq/proxy/dynamic/cglib/Action.java
+++ src/main/java/com/yq/proxy/dynamic/cglib/Action.java
@@ -4,6 +4,11 @@ package com.yq.proxy.dynamic.cglib;
  * 需要被代理的对象，cglib动态代理不需要实现接口
  */
 public class Action {
+    /**
+     * 对象转字符串
+     * @param obj
+     * @return
+     */
     public String object2String(Object obj) {
         System.out.println("进入了被代理的方法：object2String");
 
@@ -14,6 +19,10 @@ public class Action {
         return obj.toString();
     }
 
+    /**
+     * 打印对象
+     * @param obj
+     */
     public void printStr(Object obj) {
         System.out.println("进入了被代理的方法：printStr");
         System.out.println(obj);
